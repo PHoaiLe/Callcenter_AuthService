5e346ad41064778cb2763fcc4fb910eb6be1cfde
PHoaiLe <lehoaiphuong13042002@gmail.com>
2024-09-19 14:02:00 +0700

    Apply .env file for application.yml

--- src/main/java/com/callcenter/AuthService/AuthServiceApplication.java
+++ src/main/java/com/callcenter/AuthService/AuthServiceApplication.java
@@ -3,15 +3,15 @@ package com.callcenter.AuthService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAsync
 @RestController
 public class AuthServiceApplication {
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		System.out.println(args);
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
